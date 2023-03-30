fadcd977ad2609f7b498e1e2cb28f7cd93f8f3fc
Documenter.jl <documenter@juliadocs.github.io>
2023-03-30 02:03:14 +0000

    build based on a1cae17

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EllipseSampling","category":"page"},{"location":"#EllipseSampling","page":"Home","title":"EllipseSampling","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EllipseSampling.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EllipseSampling]","category":"page"}]
+}
