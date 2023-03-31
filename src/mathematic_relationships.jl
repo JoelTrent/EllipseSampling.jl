55cc4ee7f8ccab42527726c504f0c9e28e81f666
JoelTrent <79883375+JoelTrent@users.noreply.github.com>
2023-03-31 19:43:51 +1300

    Better sized brackets in math documentation.

--- src/mathematic_relationships.jl
+++ src/mathematic_relationships.jl
@@ -1,20 +1,24 @@
 """
     eccentricity_squared(a::T, b::T) where T<:Float64
 
-Computes `m`, the eccentricity of the ellipse squared, ``m=e^2``, where ``m=1-(\\frac{b}{a})^2``.
+Computes `m`, the eccentricity of the ellipse squared, ``m=e^2``, where ``m=1-\\big(\\frac{b}{a}\\big)^2``.
 
 # Arguments
 - `a`: the major radius of the ellipse.
 - `b`: the minor radius of the ellipse.
 
 # Details
-This relationship between m and e is seen by considering the equation for e: ``e=\\frac{c}{a}``, where ``c^2=(a^2-b^2)`` and ``a>b``.
+This relationship between m and e is seen by considering the equation for e: ``e=\\frac{c}{a}``, where ``c^2=\\big(a^2-b^2\\big)`` and ``a>b``.
 
-Replacing c in the equation for e: ``e=\\frac{\\sqrt{a^2-b^2}}{a}``.
+Replacing c in the equation for e: 
+    
+```math
+e=\\frac{\\sqrt{a^2-b^2}}{a}
+```
 
 Substituting the equation for e into m: 
 ```math
-m = (\\frac{\\sqrt{a^2-b^2}}{a})^2 = \\frac{a^2-b^2}{a^2} = 1 - \\frac{b^2}{a^2} = 1 - (\\frac{b}{a})^2
+m = \\bigg(\\frac{\\sqrt{a^2-b^2}}{a}\\bigg)^2 = \\frac{a^2-b^2}{a^2} = 1 - \\frac{b^2}{a^2} = 1 - \\bigg(\\frac{b}{a}\\bigg)^2
 ```
 """
 function eccentricity_squared(a::T, b::T) where T<:Float64
