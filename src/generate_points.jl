58277b6a1ea0078d66de03bb1b9aa557efda9a32
JoelTrent <79883375+JoelTrent@users.noreply.github.com>
2023-05-02 16:21:31 +1200

    Add documentation for `generate_N_clustered_points`

--- src/generate_points.jl
+++ src/generate_points.jl
@@ -151,8 +151,7 @@ function generate_perimeter_point(norm_distance_on_perimeter::Float64, Γ::Matri
 end
 
 """
-    generate_N_equally_spaced_points(num_points::Int, e::Ellipse; 
-        start_point_shift::Float64=rand())
+    generate_N_equally_spaced_points(num_points::Int, e::Ellipse; start_point_shift::Float64=rand())
 
 Generates `num_points` equally spaced points on an ellipse defined by the parameters contained within `e`. 
 
@@ -214,8 +213,7 @@ function generate_N_equally_spaced_points(num_points::Int, x_radius::T, y_radius
 end
 
 """
-    generate_N_equally_spaced_points(num_points::Int, Γ::Matrix{Float64}, θmle::Vector{Float64}, ind1::Int, ind2::Int; 
-        confidence_level::Float64=0.01, start_point_shift::Float64=rand())
+    generate_N_equally_spaced_points(num_points::Int, Γ::Matrix{Float64}, θmle::Vector{Float64}, ind1::Int, ind2::Int; confidence_level::Float64=0.01, start_point_shift::Float64=rand())
 
 An alternative way to call [`generate_N_equally_spaced_points(num_points::Int, e::Ellipse; start_point_shift::Float64=rand())`](@ref), by supplying a square matrix Γ, the inverse of the Hessian of a log-likelihood function at its maximum likelihood estimate, indexes of the two variables of interest and the confidence level that represent a 2D ellipse approximation of the log-likelihood function.
 
@@ -239,11 +237,26 @@ function generate_N_equally_spaced_points(num_points::Int, Γ::Matrix{Float64}, 
 end
 
 """
-generate_N_clustered_points(num_points::Int, 
-                            e::Ellipse; 
-                            start_point_shift::Float64=rand(),
-                            sqrt_distortion::Float64=0.0)
+generate_N_clustered_points(num_points::Int, e::Ellipse; start_point_shift::Float64=rand(), sqrt_distortion::Float64=0.0)
 
+
+Generates `num_points` spaced points on an ellipse defined by the parameters contained within `e`. 
+
+# Arguments
+- `num_points`: A positive integer number of points to generate that are spaced on the ellipse. 
+- `e`: A valid [`Ellipse`](@ref) struct which defines an ellipse.
+
+# Keyword Arguments
+- `start_point_shift`: A number ∈ [0,1]. Default is `rand()` (defined on [0,1]), meaning that, by default, every time this function is called a different set of points will be generated.
+- `sqrt_distortion`: A number ∈ [0,1]. Default is `0.0`, meaning that, by default, this function will evenly space points on the the ellipse `e` with respect to the parameter `t`.
+
+# Details
+
+Points are sampled in the same fashion as in [`generate_N_equally_spaced_points(num_points::Int, e::Ellipse; start_point_shift::Float64=rand())`](@ref), except that the spacing of them is dependent on the parameter `sqrt_distortion`. If `sqrt_distortion == 1.0` then these points are equally spaced with respect to the arc length. If `sqrt_distortion == 0.0` then these points are equally spaced with respect to the parameter `t` in the parameteric equations for the x and y locations on an ellipse ([`x_parametric_equation(t::T, e::Ellipse) where T<:Float64`](@ref) and [`y_parametric_equation(t::T, e::Ellipse) where T<:Float64`](@ref)). Varying this parameter between `0.0` and `1.0` allows all of the spacings between these two extremes to be obtained.
+
+The impact of using this function is that for all values of `sqrt_distortion < 1.0`, points generated will be more clustered around the vertexes of the ellipse on the major axis (i.e. the region of greatest curvature). The strength of this clustering increases as `sqrt_distortion` ``\\to 0.0``. The strength of the clustering is also dependent on the relative magnitudes of the major and minor axis radii. If the major and minor axis have the same radii, then the ellipse is a circle and no clustering will be observed, irrespective of the value of `sqrt_distortion`. As the magnitude of the major axis radius increases relative to the minor axis radius, the strength of the clustering will increase. This effect is valuable when using an ellipse as a starting point to find a new level set in 2D (that may be an ellipse) that contains the starting ellipse. If we seek find the new level set by pushing out from the starting ellipse tangentially at each of the generated points then the points that diverge the fastest are located at the region of greatest curvature. If generated points are equally spaced with respect to arc length, then the new level set is likely to be well defined in regions that are approximately parallel to the major axis of the starting ellipse (and with length on a parallel line of similar length to the major axis, particularly for starting ellipses with a significantly larger major axis), but poorly defined in all other regions. The clustering effect is then valuable as it helps to better define the new level set.   
+
+The function works by defining a new ellipse, `e_new` with minor axis radius equal to the supplied ellipse's minor axis radius (`e_new.b == e.b`) and major axis radius as a function of the supplied ellipse's, `e`, major and minor axis radii and the parameter `sqrt_distortion`. Namely, `e_new.a == e.b + sqrt_distortion^2 * (e.a - e.b)`. `e_new` is contained within `e` and can be varied between a circle and the supplied ellipse using the parameter `sqrt_distortion` ∈ [0,1]. After defining `e_new` we determine the angle parameters `t_vector` that equally spaces `num_points` on `e_new` with respect to arc length and then find the points on `e` parameterised by each element in `t_vector`.
 """
 function generate_N_clustered_points(num_points::Int, 
                                     e::Ellipse; 
@@ -261,7 +274,7 @@ function generate_N_clustered_points(num_points::Int,
 
     elseif sqrt_distortion == 0.0 # circle
         shift = start_point_shift/num_points
-        angles = collect(LinRange(shift, (1+shift)*2*pi, num_points+1)[1:(end-1)])
+        angles = collect(LinRange(shift, (1+shift)*2.0*pi, num_points+1)[1:(end-1)])
     else
         d_a = (e.b + sqrt_distortion^2*(e.a-e.b))
         d_e = construct_ellipse(d_a, e.b)
@@ -283,6 +296,23 @@ function generate_N_clustered_points(num_points::Int,
     return points
 end
 
+"""
+    generate_N_clustered_points(num_points::Int, x_radius::T, y_radius::T, α::T=0.0, Cx::T=0.0, Cy::T=0.0; start_point_shift::Float64=rand(), sqrt_distortion::Float64=0.0) where T<:Float64
+
+An alternative way to call [`generate_N_clustered_points(num_points::Int, e::Ellipse; start_point_shift::Float64=rand(), sqrt_distortion::Float64=0.)`](@ref), by supplying the parameters of the ellipse to generate points on.
+
+# Arguments
+- `num_points`: A positive integer number of points to generate that are equally spaced on the ellipse. 
+- `x_radius`: radius of the ellipse in the x axis (i.e. when the rotation, `α`, is zero).
+- `y_radius`: radius of the ellipse in the y axis (i.e. when the rotation, `α`, is zero).
+- `α`: an angle in radians (0 to 2π) that the ellipse has been rotated by. A positive value represents an anti-clockwise rotation. Default is `0.0`.
+- `Cx`: the x coordinate of the centre of the ellipse (the translation of the ellipse in the x axis). Default is `0.0`.
+- `Cy`: the y coordinate of the centre of the ellipse (the translation of the ellipse in the y axis). Default is `0.0`.
+
+# Keyword Arguments
+- `start_point_shift`: A number ∈ [0,1]. Default is `rand()` (defined on [0,1]), meaning that, by default, every time this function is called a different set of points will be generated.
+- `sqrt_distortion`: A number ∈ [0,1]. Default is `0.0`, meaning that, by default, this function will evenly space points on the the ellipse `e` with respect to the parameter `t`.
+"""
 function generate_N_clustered_points(num_points::Int, x_radius::T, y_radius::T, α::T=0.0, Cx::T=0.0, Cy::T=0.0; start_point_shift::Float64=rand(), sqrt_distortion::Float64=0.0) where T<:Float64
 
     e = construct_ellipse(x_radius, y_radius, α, Cx, Cy)
@@ -290,7 +320,25 @@ function generate_N_clustered_points(num_points::Int, x_radius::T, y_radius::T, 
                                         sqrt_distortion=sqrt_distortion)
 end
 
-function generate_N_clustered_points(num_points::Int, Γ::Matrix{Float64}, θmle::Vector{Float64}, ind1::Int, ind2::Int; confidence_level::Float64=0.01, start_point_shift::Float64=rand(), sqrt_distortion::Float64=0.0)
+"""
+    generate_N_clustered_points(num_points::Int, Γ::Matrix{Float64}, θmle::Vector{Float64}, ind1::Int, ind2::Int; confidence_level::Float64=0.01, start_point_shift::Float64=rand(), sqrt_distortion::Float64=0.0)
+
+An alternative way to call [`generate_N_clustered_points(num_points::Int, e::Ellipse; start_point_shift::Float64=rand(), sqrt_distortion::Float64=0.)`](@ref), by supplying a square matrix Γ, the inverse of the Hessian of a log-likelihood function at its maximum likelihood estimate, indexes of the two variables of interest and the confidence level that represent a 2D ellipse approximation of the log-likelihood function.
+
+# Arguments
+- `num_points`: A positive integer number of points to generate that are equally spaced on the ellipse. 
+- `Γ`: A square matrix (2D) which is the inverse of the Hessian of a log-likelihood function at its maximum likelihood estimate.
+- `θmle`: The maximum likelihood estimate for the parameters.
+- `ind1`: Index of the first parameter of interest (corresponds to the row and column index of `Γ`)
+- `ind2`: Index of the second parameter of interest (corresponds to the row and column index of `Γ`).
+
+# Keyword Arguments
+- `confidence_level`: The confidence level ∈[0.0,1.0] at which the ellipse approximation is constructed. Default is `0.01`.
+- `start_point_shift`: A number ∈ [0,1]. Default is `rand()` (defined on [0,1]), meaning that, by default, every time this function is called a different set of points will be generated.
+- `sqrt_distortion`: A number ∈ [0,1]. Default is `0.0`, meaning that, by default, this function will evenly space points on the the ellipse `e` with respect to the parameter `t`.
+"""
+function generate_N_clustered_points(num_points::Int, Γ::Matrix{Float64}, θmle::Vector{Float64}, ind1::Int, ind2::Int; 
+    confidence_level::Float64=0.01, start_point_shift::Float64=rand(), sqrt_distortion::Float64=0.0)
 
     _, _, x_radius, y_radius, α = calculate_ellipse_parameters(Γ, ind1, ind2, confidence_level)
     return generate_N_clustered_points(num_points, x_radius, y_radius, α, θmle[ind1], θmle[ind2],
