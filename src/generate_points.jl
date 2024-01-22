bb2aa090a9977c9aa66a72cb98026633a343ab20
JoelTrent <79883375+JoelTrent@users.noreply.github.com>
2024-01-22 13:33:11 +1300

    Add `dof` kwarg to `generate_N_clustered_points` documentation definition

--- src/generate_points.jl
+++ src/generate_points.jl
@@ -382,7 +382,8 @@ function generate_N_clustered_points(num_points::Int, x_radius::T, y_radius::T, 
 end
 
 """
-    generate_N_clustered_points(num_points::Int, Γ::Matrix{Float64}, θmle::Vector{Float64}, ind1::Int, ind2::Int; confidence_level::Float64=0.01, start_point_shift::Float64=rand(), sqrt_distortion::Float64=0.0)
+    generate_N_clustered_points(num_points::Int, Γ::Matrix{Float64}, θmle::Vector{Float64}, ind1::Int, ind2::Int; 
+        confidence_level::Float64=0.01, dof::Int=2, start_point_shift::Float64=rand(), sqrt_distortion::Float64=0.0)
 
 An alternate way to call [`generate_N_clustered_points(num_points::Int, e::Ellipse; start_point_shift::Float64=rand(), sqrt_distortion::Float64=0.)`](@ref), by supplying a square matrix Γ, the inverse of the Hessian of a log-likelihood function at its maximum likelihood estimate, indexes of the two variables of interest and the confidence level that represent a 2D ellipse approximation of the log-likelihood function.
 
