96be7ffd3e185ff991297e083cb47da45ba09283
JoelTrent <79883375+JoelTrent@users.noreply.github.com>
2024-01-22 13:30:09 +1300

    Add specification of the degrees of freedom, `dof`, used to define the boundary of the ellipse approximation of a log-likelihood function

--- src/generate_points.jl
+++ src/generate_points.jl
@@ -395,13 +395,14 @@ An alternate way to call [`generate_N_clustered_points(num_points::Int, e::Ellip
 
 # Keyword Arguments
 - `confidence_level`: the confidence level ∈ [0.0,1.0] at which the ellipse approximation is constructed. Default is `0.01`.
+- `dof`: integer degrees of freedom used for calculation of the asymptotic confidence threshold defining the ellipse. Default is `2`.
 - `start_point_shift`: a number ∈ [0.0,1.0]. Default is `rand()` (defined on [0.0,1.0]), meaning that, by default, every time this function is called a different set of points will be generated.
 - `sqrt_distortion`: a number ∈ [0.0,1.0]. Default is `0.0`, meaning that, by default, this function will evenly space points on the the ellipse `e` with respect to the parameter `t`.
 """
 function generate_N_clustered_points(num_points::Int, Γ::Matrix{Float64}, θmle::Vector{Float64}, ind1::Int, ind2::Int; 
-    confidence_level::Float64=0.01, start_point_shift::Float64=rand(), sqrt_distortion::Float64=0.0)
+    confidence_level::Float64=0.01, dof::Int=2, start_point_shift::Float64=rand(), sqrt_distortion::Float64=0.0)
 
-    _, _, x_radius, y_radius, α = calculate_ellipse_parameters(Γ, ind1, ind2, confidence_level)
+    _, _, x_radius, y_radius, α = calculate_ellipse_parameters(Γ, ind1, ind2, confidence_level, dof)
     return generate_N_clustered_points(num_points, x_radius, y_radius, α, θmle[ind1], θmle[ind2],
                                     start_point_shift=start_point_shift, sqrt_distortion=sqrt_distortion)
 end
